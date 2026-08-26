0c76e9ba3fc3d8999b06964d7814c9923aa99601
mariejcse <mriejxse@gmail.com>
2026-08-26 03:43:17 +0100

    Eliminar carpeta pages

--- src/app/pages/login/login.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  imports: [],
-  selector: 'app-login',
-  styleUrl: './login.css',
-  templateUrl: './login.html',
-})
-export class Login {}
