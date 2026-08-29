dcda3c60986094b4dc40bc238c6276d07c09d44c
Robin Tejada <robintejadakinal@gmail.com>
2026-08-28 21:11:03 -0600

    restuaracion de ChatBot

--- src/app/modules/farmacia/farmacia.ts
+++ src/app/modules/farmacia/farmacia.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { CarritoService } from '../../services/carrito';
+import { FormsModule } from '@angular/forms';
 
 export interface Medicamento {
   id_medicamento: number;
@@ -15,7 +16,7 @@ export interface Medicamento {
 @Component({
   selector: 'app-farmacia',
   standalone: true,
-  imports: [CommonModule],
+ imports: [CommonModule, FormsModule],
   templateUrl: './farmacia.html',
   styleUrl: './farmacia.css'
 })
@@ -55,5 +56,63 @@ export class FarmaciaComponent implements OnInit {
   });
 
   console.log('Producto agregado al carrito:', medicamento.nombre);
+}
+
+// --- ASISTENTE VIRTUAL VOLTY ---
+chatAbierto: boolean = false;
+mensajeUsuario: string = '';
+
+historialMensajes = [
+  {
+    emisor: 'asistente',
+    texto: '¡Hola! Soy Volty, tu asistente de la Farmacia Voltstack. ¿En qué puedo ayudarte hoy con tus medicamentos?'
+  }
+];
+
+alternarChat(): void {
+  this.chatAbierto = !this.chatAbierto;
+}
+
+enviarMensajeChat(): void {
+  if (!this.mensajeUsuario.trim()) return;
+
+  this.historialMensajes.push({
+    emisor: 'usuario',
+    texto: this.mensajeUsuario
+  });
+
+  const consulta = this.mensajeUsuario.toLowerCase();
+  this.mensajeUsuario = '';
+
+  setTimeout(() => {
+    let respuestaBot =
+      'Entiendo tu consulta. Para brindarte información exacta sobre dosis o recetas reguladas, por favor facilítame el nombre del medicamento o comunícate con un asesor clínico.';
+
+    if (
+      consulta.includes('paracetamol') ||
+      consulta.includes('ibuprofeno') ||
+      consulta.includes('dolor')
+    ) {
+      respuestaBot =
+        'Contamos con analgésicos disponibles como Paracetamol e Ibuprofeno en stock de entrega inmediata sin receta.';
+    } else if (
+      consulta.includes('receta') ||
+      consulta.includes('seguro')
+    ) {
+      respuestaBot =
+        'Para medicamentos controlados, puedes cargar tu receta médica al momento de finalizar tu orden de compra en línea.';
+    } else if (
+      consulta.includes('horario') ||
+      consulta.includes('tiempo')
+    ) {
+      respuestaBot =
+        'Nuestra farmacia central atiende las 24 horas del día. Los despachos a domicilio toman un estimado de 30 a 45 minutos.';
+    }
+
+    this.historialMensajes.push({
+      emisor: 'asistente',
+      texto: respuestaBot
+    });
+  }, 600);
 }
 }
